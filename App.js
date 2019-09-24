62b01f19dd2a17ad7060ebfc5a318616f8b86eeb
Nawaf ALrifai <nbalrifai@gmail.com>
2019-09-24 16:14:07 +0300

    cleanup

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { Component } from "react";
 import { StyleSheet } from "react-native";
 import AppContainer from "./navigation";
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return <AppContainer />;
   }
